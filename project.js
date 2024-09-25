550d9c5450da52250cc2449de239db2d7e5d8683
Pushpendu Dey <156361232+Pushpendu197@users.noreply.github.com>
2024-09-25 13:47:31 +0530

    app

---
+++ project.js
@@ -0,0 +1,70 @@
+const BASE_URL = 'https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies'
+
+const dropdowns = document.querySelectorAll('.dropdown select');
+const btn = document.querySelector('form button');
+const fromCurr = document.querySelector('.from select');
+const toCurr = document.querySelector('.to select');
+const msg = document.querySelector(".msg");
+
+for (let select of dropdowns) {
+    for (currCode in countryList) {
+        console.log(currCode, countryList[currCode]);
+
+        let newOption = document.createElement("option")
+        newOption.innerText = currCode;
+        newOption.value = currCode;
+
+
+        //selected USD and INR by default 
+        if (select.name === "from" && currCode === "USD") {
+            newOption.selected === "selected";
+        } else if (select.name === "to" && currCode === "INR") {
+            newOption.selected === "selected";
+        }
+
+        select.append(newOption);
+
+    }
+
+    select.addEventListener('change', (evt) => {
+        updateFlag(evt.target);
+    })
+}
+
+
+const updateFlag = (element) => {
+    let currCode = element.value
+    console.log(currCode);
+
+    let countryCode = countryList[currCode]; //IN , BDT etc 
+    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`
+    let img = element.parentElement.querySelector('img');
+    img.src = newSrc;
+
+}
+
+
+btn.addEventListener('click', async (evt) => {
+    evt.preventDefault();
+
+    let amount = document.querySelector(".amount input");
+    let amtVal = amount.value;
+
+    if (amtVal === "" || amtVal < 0) {
+        amtVal = 1;
+        amount.value = 1;
+    }
+    console.log(amtVal);
+    console.log(fromCurr.value, toCurr.value);
+
+
+    const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}.json`;
+    let response = await fetch(URL);
+    let data = await response.json();
+    let counCodes = data[fromCurr.value.toLowerCase()];
+    let exchange_rate = counCodes[toCurr.value.toLowerCase()];
+    let finalAmount = amtVal * exchange_rate;
+    msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+
+
+})
